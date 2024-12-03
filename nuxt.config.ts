88cef418a66fada4dc04978953219e3c3763b1f4
Ahmed Gharib <ahmed@trinergydigital.com>
2024-12-03 15:09:15 +0800

    forth commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: [
-    'bootstrap/dist/css/bootstrap.min.css',
-  ],
   app: {
     head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
+        },
+      ],
       script: [
         {
-          src: 'https://kit.fontawesome.com/e811ef59e0.js',
-          crossorigin: 'anonymous',
-        },
-        {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js',
-          crossorigin: 'anonymous',
+          defer: true,
         },
       ],
     },

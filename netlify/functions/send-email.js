cc557a2d6a1f29f5ccdf7257f94bdc3298ec7930
Ahmed Gharib <ahmed@trinergydigital.com>
2024-12-03 16:51:45 +0800

    form debugging attempt 7

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -18,7 +18,7 @@ export default defineEventHandler(async (event) => {
     secure: true,
     auth: {
       user: "ahmedgharo@gmail.com",
-      pass: "ivdxnwcqvzxazxqp",
+      pass: "ivdx nwcq vzxa zxqp",
     },
   });
 

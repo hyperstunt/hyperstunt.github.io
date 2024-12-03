1550841e3def126ecfd382f38ab52c4468d80580
Ahmed Gharib <ahmed@trinergydigital.com>
2024-12-03 15:16:45 +0800

    fifth commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  nitro: {
+    preset: "static",
+  },
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
     head: {
       link: [
         {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
         },
       ],
       script: [
         {
-          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js',
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",
           defer: true,
         },
       ],

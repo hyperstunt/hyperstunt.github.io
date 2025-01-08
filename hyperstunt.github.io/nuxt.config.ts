9fb364a892bf80baa1e2579136021b3e19ebf3bd
Ahmed Gharib <ahmed@trinergydigital.com>
2025-01-08 12:06:53 +0800

    added tab logo

--- hyperstunt.github.io/nuxt.config.ts
+++ hyperstunt.github.io/nuxt.config.ts
@@ -12,6 +12,11 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
         },
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/img/AG.jpg",
+        },
       ],
       script: [
         {
